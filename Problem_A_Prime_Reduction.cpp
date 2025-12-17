79c793c51af867f862b6f0ac763a22778410d162
rui <112938973+MountainR@users.noreply.github.com>
2025-12-17 13:23:19 +0000

    A: generate all primes

---
+++ Problem_A_Prime_Reduction.cpp
@@ -0,0 +1,38 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+// generate all primes
+vector<int> eratosthenes(int n){
+    bool *isMarked = new bool[n+1];
+    memset(isMarked, 0, n+1);
+    vector<int> primes;
+    
+    int i = 2;
+    for(; i*i <= n; ++i)
+        if (!isMarked[i]) {
+            primes.push_back(i);
+            for(int j = i; j <= n; j += i)
+                isMarked[j] = true;
+        }
+    for (; i <= n; i++)
+        if (!isMarked[i])
+            primes.push_back(i);
+    return primes;
+}
+// check if x is prime
+// find x's prime factors
+// prime reduction
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    return 0;
+}
