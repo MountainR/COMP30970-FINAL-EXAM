c55670108a92fa921025148d5115c36c1cd3fa70
rui <112938973+MountainR@users.noreply.github.com>
2025-12-17 16:34:48 +0000

    E: start the problem

---
+++ Problem_E_Bread_Sorting.cpp
@@ -0,0 +1,47 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+
+bool compare(const vector<int> &breads, const vector<int> &sorted){
+    for(int i = 0; i < breads.size(); ++i){
+        if(breads[i] != sorted[i]){return false;}
+    }
+    return true;
+}
+
+// search in a graph. dfs
+bool isPossible(vector<int> &breads, const vector<int> &sorted){
+    if(compare(breads, sorted)){return true;}   // base case
+    else{
+        
+    }  
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // get inputs
+    int N;
+    cin >> N;
+    vector<int> breads(N), sorted(N);
+    for(int i = 0; i < N; ++i){
+        cin >> breads[i];
+    }
+    for(int i = 0; i < N; ++i){
+        cin >> sorted[i];
+    }
+
+    if(isPossible(breads, sorted)) cout << "Possible";
+    else cout << "Impossible";
+
+    return 0;
+}
